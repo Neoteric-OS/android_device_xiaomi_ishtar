a804c7eab9cec91905e1d2eaecfb56055871aa92
Arian <arian.kulmer@web.de>
2023-08-23 20:20:26 +0200

    ishtar: Initial sepolicy and enforcing selinux
    Change-Id: Ia21793576649e8518e79e4680e0b79b6a9331720

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,6 +183,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor boot
