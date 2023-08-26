b816bcea53b486dc9cb1f01bce2d1ed6bf737b4c
IPSBHANGU <inderpreetbhangu2@gmail.com>
2023-08-26 13:30:24 +0200

    ishtar: Build gatekeeper, keymaster and keymint packages

--- kalama.mk
+++ kalama.mk
@@ -184,6 +184,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0.vendor
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
@@ -198,6 +202,21 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.odm.rc \
     ueventd.qcom.rc
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1.vendor \
+    libkeymaster_messages.vendor
+
+# Keymint
+PRODUCT_PACKAGES += \
+    android.hardware.hardware_keystore.xml \
+    android.hardware.security.keymint-V1-ndk.vendor \
+    android.hardware.security.secureclock-V1-ndk.vendor \
+    android.hardware.security.sharedsecret-V1-ndk.vendor
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
 
 # Overlays
 PRODUCT_PACKAGES += \
