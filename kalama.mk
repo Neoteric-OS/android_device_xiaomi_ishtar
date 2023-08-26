835fe5f9acd24b62830d74c9a6f196893c6a0416
Akash Srivastava <akashniki@gmail.com>
2023-08-26 12:23:04 +0200

    ishtar: Build authsecret

--- kalama.mk
+++ kalama.mk
@@ -105,6 +105,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Authsecret
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret@1.0.vendor
+
 # Board
 TARGET_BOARD_PLATFORM := kalama
 
