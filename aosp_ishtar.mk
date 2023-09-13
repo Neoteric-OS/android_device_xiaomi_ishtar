421831dff8439426506016d37fb799cc1f77675c
Akash Srivastava <akashniki@gmail.com>
2023-09-13 19:23:41 +0200

    ishtar: Pixel Experience additions

--- aosp_ishtar.mk
+++ aosp_ishtar.mk
@@ -11,6 +11,11 @@ $(call inherit-product, $(DEVICE_PATH)/kalama.mk)
 
 # Inherit common Pixel Experience configurations
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product-if-exists, vendor/pixel-additional/config.mk)
+
+# Pixel Experience additions
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_CARRIER_SETTINGS := true
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1440
