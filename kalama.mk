2c649bcb46abbdcc57c4a68344a1fdd4b215043a
Akash Srivastava <akashniki@gmail.com>
2023-09-17 21:22:46 +0200

    ishtar: Switch to prebuilt thermal HAL
    * OSS ones doesn't play well with charging temps. Charging temps are around 60° which is way higher than it should be. Prebuilt one works good together with mi_thermal

--- kalama.mk
+++ kalama.mk
@@ -501,8 +501,7 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0 \
-    android.hardware.thermal@2.0.vendor \
-    android.hardware.thermal@2.0-service.qti-v2
+    android.hardware.thermal@2.0.vendor
 
 # Trusted User Interface (TUI)
 PRODUCT_PACKAGES += \
