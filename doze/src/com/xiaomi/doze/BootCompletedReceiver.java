c1ef811e1b18f6043c800fdc41e190480a87ee09
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-10-12 07:50:41 +0200

    ishtar: doze: Override HDR types for dolby vision
    Stock display HAL doesn't enable dolby vision support,
    instead miui uses a custom surfaceflinger lib for the
    same. Since we won't be using that, simply override
    supported HDR types at boot.

--- doze/src/com/xiaomi/doze/BootCompletedReceiver.java
+++ doze/src/com/xiaomi/doze/BootCompletedReceiver.java
@@ -21,6 +21,9 @@ package com.xiaomi.doze;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.IBinder;
+import android.view.Display.HdrCapabilities;
+import android.view.SurfaceControl;
 import android.util.Log;
 
 import com.xiaomi.doze.DozeUtils;
@@ -34,5 +37,12 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
+
+        // Override HDR types
+        final IBinder displayToken = SurfaceControl.getInternalDisplayToken();
+        SurfaceControl.overrideHdrTypes(displayToken, new int[]{
+               HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
+               HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
+
     }
 }
