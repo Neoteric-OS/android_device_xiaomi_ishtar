be0deb89239b1af8c607f14170e070fa889bb708
Akash Srivastava <akashniki@gmail.com>
2024-12-20 01:10:00 +0100

    ishtar: Update blobs to HyperOS 1.0.20

--- ishtar.mk
+++ ishtar.mk
@@ -18,8 +18,8 @@ PRODUCT_MODEL := Xiaomi 13 Ultra
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="ishtar_global-user 14 UKQ1.230804.001 V816.0.16.0.UMACNXM release-keys"
+    PRIVATE_BUILD_DESC="ishtar_global-user 14 UKQ1.230804.001 V816.0.20.0.UMACNXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/ishtar_global/ishtar:14/UKQ1.230804.001/V816.0.16.0.UMACNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/ishtar_global/ishtar:14/UKQ1.230804.001/V816.0.20.0.UMACNXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
